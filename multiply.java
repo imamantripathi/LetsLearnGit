6a2096d98898c44e9caed842d2060bff4b2b0783
Aman Tripathi <imamantripathi@gmail.com>
2021-09-18 11:21:38 +0530

    adding multiply file

---
+++ multiply.java
@@ -0,0 +1,11 @@
+
+public class multiply
+{
+    public static void main(String args[])
+    {
+        int a=3;
+        int b=2;
+        int c= a*b;
+        System.out.println("the sum od A and B "+c);
+    }
+}
